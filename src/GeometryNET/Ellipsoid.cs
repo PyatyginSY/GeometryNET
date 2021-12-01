ebaa0641ee43c7348baa0baf15393860b0da2aa6
PyatyginSY <PyatyginSY@gmail.com>
2021-12-01 08:23:42 +0400

    feat: add Ellipsoid and EllipsoidTests

---
+++ src/GeometryNET/Ellipsoid.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="Ellipsoid.cs" company="Pyatygin S.Y.">
+// Copyright (c) Pyatygin S.Y.. All rights reserved.
+// </copyright>
+
+using GeometryNET.Exceptions;
+using System;
+
+namespace GeometryNET
+{
+    /// <summary>
+    /// Geometric parameters of the Ellipsoid.
+    /// </summary>
+    public static class Ellipsoid
+    {
+        /// <summary>
+        /// Returns a ellipsoid volume.
+        /// </summary>
+        /// <param name="halfAxisHeight">Ellipsoid height half-axis.</param>
+        /// <param name="halfAxisWidth">Ellipsoid width half-axis.</param>
+        /// <param name="halfAxisDeflection">Ellipsoid deflection half-axis.</param>
+        /// <returns>Ellipsoid volume.</returns>
+        public static double GetVolume(double halfAxisHeight, double halfAxisWidth, double halfAxisDeflection)
+        {
+            if (halfAxisHeight < 0 || halfAxisWidth < 0 || halfAxisDeflection < 0)
+            {
+                throw new GeometryArgumentException("Parameter 'halfAxisHeight' or 'halfAxisWidth'or 'halfAxisDeflection' cannot be negative");
+            }
+
+            return (4.0 / 3.0) * Math.PI * halfAxisHeight * halfAxisWidth * halfAxisDeflection;
+        }
+    }
+}
---
+++ tests/GeometryNET.Tests/EllipsoidTests.cs
@@ -0,0 +1,47 @@
+﻿// <copyright file="EllipsoidTests.cs" company="Pyatygin S.Y.">
+// Copyright (c) Pyatygin S.Y.. All rights reserved.
+// </copyright>
+
+using GeometryNET.Exceptions;
+using Xunit;
+
+namespace GeometryNET.Tests
+{
+    /// <summary>
+    /// Tests class Ellipsoid.
+    /// </summary>
+    public class EllipsoidTests
+    {
+        /// <summary>
+        /// A test for Ellipsoid.GetVolume.
+        /// </summary>
+        [Fact]
+        public void GetVolumeEllipsoidParametersReturnVolume()
+        {
+            const double HalfAxisHeight = 1;
+            const double HalfAxisWidth = 2;
+            const double HalfAxisDeflection = 3;
+
+            var actual = Ellipsoid.GetVolume(HalfAxisHeight, HalfAxisWidth, HalfAxisDeflection);
+
+            Assert.Equal(25.132741228718345, actual);
+        }
+
+        /// <summary>
+        /// A test for Ellipsoid.GetVolume.
+        /// </summary>
+        /// <param name="halfAxisHeight">halfAxisHeight.</param>
+        /// <param name="halfAxisWidth">halfAxisWidth.</param>
+        /// <param name="halfAxisDeflection">halfAxisDeflection.</param>
+        [Theory]
+        [InlineData(-1, 1, 1, "Parameter 'halfAxisHeight' or 'halfAxisWidth'or 'halfAxisDeflection' cannot be negative")]
+        [InlineData(1, -1, 1, "Parameter 'halfAxisHeight' or 'halfAxisWidth'or 'halfAxisDeflection' cannot be negative")]
+        [InlineData(1, 1, -1, "Parameter 'halfAxisHeight' or 'halfAxisWidth'or 'halfAxisDeflection' cannot be negative")]
+        public void GetVolumeEllipsoidParametersReturnThrow(double halfAxisHeight, double halfAxisWidth, double halfAxisDeflection, string expected)
+        {
+            var actual = Assert.Throws<GeometryArgumentException>(() => Ellipsoid.GetVolume(halfAxisHeight, halfAxisWidth, halfAxisDeflection));
+
+            Assert.Equal(expected, actual.Message);
+        }
+    }
+}
